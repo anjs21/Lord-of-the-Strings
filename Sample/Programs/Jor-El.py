cc5798d86422cabfe4d01c84555079e210283ad3
OmkarBaraskar <59916487+OmkarBaraskar@users.noreply.github.com>
2020-01-25 00:58:15 +0530

    Add files via upload

---
+++ Sample/Programs/Jor-El.py
@@ -0,0 +1,50 @@
+#All the libraries and other modules and functions you import
+import multiprocessing
+def info(a,b):
+    """
+      if a == 0:
+        b=[number1, number2, number3]
+        
+        number1 Operation
+        1       Right/Left Shift
+
+        2       Alt-Swap/Half-Reverse
+
+      elif a==1:
+        b=[[label1,label2,label3],index]
+    """
+    
+    #All your Code
+
+    return None
+def move(a):
+    """
+       a = [label1,label2,label3,label4,label5]
+       Here label{i} where i in [1,5] are city labels.
+    """
+
+    #All your Code
+
+    return a
+    
+def exchange(number):
+    """
+       number Operation
+       1      Right Shift
+       2      Left Shift
+       3      Alt Swap
+       4      Half-Reverse
+
+    """
+    #All your Code
+
+    return None
+def init(a):
+    """
+       a = [label1,label2,label3,label4,label5]
+       Here label{i} where i in [1,5] are city labels.
+    """
+    #All your Code
+
+    return None
+
---
+++ Sample/Programs/Zod.py
@@ -0,0 +1,28 @@
+import random
+from Moves import  * 
+def info(a):
+    """
+        a=[label1,label2,label3,label4,label5]
+    """
+    #All your Code
+
+    return None
+def move(a,b):
+    """
+       a = [label1,label2,label3]
+       b=index
+    """
+    #All your Code
+
+    return a,b
+
+def init(a,b):
+    """
+       a = [label1,label2,label3,label4,label5]
+       b=index
+
+    """
+    #All your Code
+
+    return None
+
